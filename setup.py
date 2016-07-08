a37c945a40db73d729e6ab01aff711760b780d0c
Israel Teixeira <israelst@gmail.com>
2016-07-07 23:59:41 -0300

    Create setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dbf2csv',
+        version='0.1',
+        description='A straightforward lib to convert dbf files into csv',
+        url='http://github.com/israelst/dbf2csv',
+        author='Israel Teixeira',
+        author_email='iserael@gmail.com',
+        license='GPL3',
+        packages=['dbf2csv'],
+        zip_safe=False)
